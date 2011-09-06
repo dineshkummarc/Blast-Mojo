4ef783ef382b098a11eb3f1da4b6845c03c48503
Jaime Bueza <jbueza@gmail.com>
2011-09-05 22:44:00 -0700

    Add registration controller.

--- examples/helloworld/js/helloworld/SiteMap.js
+++ examples/helloworld/js/helloworld/SiteMap.js
@@ -2,8 +2,10 @@ dojo.provide("helloworld.SiteMap");
 helloworld.SiteMap = [
   {
     pattern: ".mojoTemplate",
-    controllers: [
-      {controller: "stdlib.controller.TemplateController"}
-    ]
+    controllers: [{ controller: "stdlib.controller.TemplateController"}]
+  },
+  {
+    pattern: "#registration-form",
+    controllers: [{ controller: "helloworld.controller.RegistrationController" }]
   }
 ];
---
+++ examples/helloworld/js/helloworld/controller/RegistrationController.js
@@ -0,0 +1,8 @@
+﻿dojo.provide("helloworld.controller.RegistrationController");
+dojo.declare("helloworld.controller.RegistrationController", mojo.controller.Controller, {
+  addObservers: function() {
+    console.log("Controller has been mapped");
+  },
+  addCommands: function() { },
+  addIntercepts: function() { }
+});
