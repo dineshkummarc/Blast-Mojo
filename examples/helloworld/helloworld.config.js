dd67c48da40b842306fa7910280fadff71cc5850
Jaime Bueza <jbueza@gmail.com>
2011-09-05 22:32:53 -0700

    Add registration form.

--- examples/helloworld/helloworld.config.js
+++ examples/helloworld/helloworld.config.js
@@ -1,8 +1,7 @@
 dojo.registerModulePath("mojo", "../../src");
 dojo.registerModulePath("helloworld", "../helloworld/js/helloworld");
-
 mojo.require("helloworld.SiteMap");
-//dojo.extend(mojo, dojo);
+
 dojo.addOnLoad(function() {
   var ctrlIniter = mojo.controller.Map.getInstance();
   ctrlIniter.setSiteMap(helloworld.SiteMap);
